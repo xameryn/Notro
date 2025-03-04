585bef863c72c862e89b9d3fa5c55f1fdc71d2c3
Benjamin <benjaminjamesweb@gmail.com>
2025-03-04 14:05:03 -0800

    little UI changes

--- src/pages/ConnectPage.jsx
+++ src/pages/ConnectPage.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import './styles/ConnectPage.css'
 
 const ConnectPage = () => {
 
   const navigate = useNavigate()
-  
+
   return (
-    <div>
-            <h1>Connect to Notro Using Discord</h1>
-            <button>CONNECT</button>
-            <button className="temp-red-background" onClick={() => navigate('/')}>SKIP AND GO STRAIGHT TO HOMEPAGE</button>
+    <div className='connect-div'>
+        <h1>Connect to Notro Using Discord</h1>
+        <button>CONNECT</button>
+        <button className="temp-red-background" onClick={() => navigate('/')}>SKIP AND GO STRAIGHT TO HOMEPAGE</button>
     </div>
   )
 }
