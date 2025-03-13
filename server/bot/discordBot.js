592be8face7c8d7d44319d04235f2c1099fa2258
Arn <Arlexan836@gmail.com>
2025-03-12 19:07:31 -0700

    Discord imbed

--- server/bot/discordBot.js
+++ server/bot/discordBot.js
@@ -1,6 +1,7 @@
 
-import {Client, IntentsBitField} from "discord.js";
+import {Client, IntentsBitField, EmbedBuilder} from "discord.js";
 import dotenv from 'dotenv';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -27,11 +28,54 @@ client.on('messageCreate', (message) => {
     }
 })
 
-client.on('interactionCreate', (interaction) => {
+
+async function fetchGif(query) {
+    const url = `https://tenor.googleapis.com/v2/search?q=${query}&key=${process.env.API_KEY}&limit=50&media_filter=minimal`;
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if(!data.results || data.results.length === 0){
+            return null;
+        }
+
+        const randomIndex = Math.floor(Math.random() * data.results.length)
+        return data.results[randomIndex].media_formats.gif.url;
+    } catch (error) {
+        console.error("Error fetching GIF:", error);
+        return null;
+    }
+}
+
+client.on('interactionCreate', async(interaction) => {
     if(!interaction.isChatInputCommand()) return;
 
-    if(interaction.commandName === "ping"){
-        interaction.reply("Pong!")
+    if(interaction.commandName === 'ping'){
+        interaction.reply('Pong!')
+    }
+
+    if(interaction.commandName === "gif"){
+
+        // Sets description empty if not provided
+        const gifDescription = interaction.options.get('description')?.value || " ";
+        const gifName = interaction.options.get('gif-name').value
+
+
+        const gifUrl = await fetchGif(gifName)
+        if(!gifUrl){
+            await interaction.reply("I must be blind cause I can't find it.")
+            return
+        }
+
+        const embed = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle(gifName)
+        .setImage(gifUrl)
+        .setDescription(gifDescription);
+        
+
+       await interaction.reply({ embeds: [embed]});
     }
 })
 
--- server/bot/registerCommands.js
+++ server/bot/registerCommands.js
@@ -1,22 +1,43 @@
 
-import {REST, Routes} from "discord.js";
+import {REST, Routes, ApplicationCommandOptionType} from "discord.js";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+
+// List of commands
 const commands = [
     {
         name: 'ping',
         description: 'Pong!'
 
+    },
+    {
+        name: 'gif',
+        description: 'gif Embed test',
+        options: [
+            {
+                name: 'gif-name',
+                description: 'Search by gif name',
+                type: ApplicationCommandOptionType.String,
+                required: true
+            },
+            {
+                name: 'description',
+                description: 'Gif description',
+                type: ApplicationCommandOptionType.String,
+                required: false
+            }
+        ]
     }
 ]
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
+
 (async() => {
     try{
-        
+        // Send request to discord to register a command to a specific server
         await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands}
