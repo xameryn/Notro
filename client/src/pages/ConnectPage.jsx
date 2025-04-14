17d4ef0839113ce90beba7bc4dda8579011ea1c4
artemgood477 <artemgood4@gmail.com>
2025-04-13 19:41:26 -0700

    use different account button + css

--- client/src/pages/ConnectPage.jsx
+++ client/src/pages/ConnectPage.jsx
@@ -4,22 +4,25 @@ import './styles/ConnectPage.css';
 const authServerUrl = import.meta.env.AUTH_SERVER_URL || 'http://localhost:4001';
 
 const ConnectPage = () => {
-  const handleDiscordLogin = () => {
-    const origin = window.location.origin;
-    const clientId = "1348936068635820063";
-    const redirectUri = `${authServerUrl}/auth/discord/callback`;
-    const scope = "identify guilds";
-    const responseType = "code";
-    const prompt = "consent";
-  
-    const relativeOauthPath = `/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=${responseType}&scope=${encodeURIComponent(scope)}&prompt=${prompt}&state=${encodeURIComponent(origin)}`;
-    const loginWrapper = `https://discord.com/login?redirect_to=${encodeURIComponent(relativeOauthPath)}`;
-  
-    window.location.href = loginWrapper;
-  };
-  
+  const origin = window.location.origin;
+  const clientId = "1348936068635820063";
+  const redirectUri = `${authServerUrl}/auth/discord/callback`;
+  const scope = "identify guilds";
+  const responseType = "code";
+  const prompt = "consent";
 
-  
+  const buildOAuthUrl = () => {
+    const relativeOauthPath = `/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=${responseType}&scope=${encodeURIComponent(scope)}&prompt=${prompt}&state=${encodeURIComponent(origin)}`;
+    return `https://discord.com/login?redirect_to=${relativeOauthPath}`;
+  };
+
+  const handleDiscordLogin = () => {
+    window.location.href = buildOAuthUrl();
+  };
+
+  const handleSwitchUser = () => {
+    window.location.href = buildOAuthUrl();
+  };
 
   return (
     <div className='connect-div'>
@@ -32,6 +35,10 @@ const ConnectPage = () => {
           <p>Connect using Discord</p>
           <img src="/discord_icon.png" alt="Discord Icon" />
         </button>
+
+        <p className="switch-account-note">
+          Not your account? <span onClick={handleSwitchUser}>Use a different one</span>
+        </p>
       </div>
     </div>
   );
